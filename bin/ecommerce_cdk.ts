0fde0205eb4b8c2636f6d57b0e0724ad8f455626
Adriano Sastre Vieira <adrianosastre@inatel.br>
2021-07-28 09:22:06 -0300

    first commit

--- bin/ecommerce_cdk.ts
+++ bin/ecommerce_cdk.ts
@@ -1,21 +1,28 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { EcommerceCdkStack } from '../lib/ecommerce_cdk-stack';
+import { PipelineStack } from '../lib/pipeline/pipeline-stack';
 
 const app = new cdk.App();
-new EcommerceCdkStack(app, 'EcommerceCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+/*const branch = 'DEV';
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+if (branch == 'DEV') {
+  account:
+  region:
+  debug:
+}
+*/
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new PipelineStack(app, 'PipelineStack', {
+  env: {
+    account: '685730834918',
+    region: 'us-east-1',
+  },
+  tags: {
+    ['cost']: 'Ecommerce',
+    ['team']: 'adrianosastre',
+  }
 });
+
+app.synth();
---
+++ lib/pipeline/pipeline-stack.ts
@@ -0,0 +1,40 @@
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipelineactions from "@aws-cdk/aws-codepipeline-actions";
+import * as cdk from "@aws-cdk/core";
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        const pipeline = new CdkPipeline(this, 'Pipeline', {
+            pipelineName: 'EcommercePipeline',
+            cloudAssemblyArtifact: cloudAssemblyArtifact,
+
+            sourceAction: new codepipelineactions.GitHubSourceAction({
+                actionName: 'GitHub',
+                output: sourceArtifact,
+                oauthToken: cdk.SecretValue.secretsManager('github-token-pipeline'),
+                owner: 'adrianosastre',
+                repo: 'EcommerceCDK',
+                branch: 'master',
+            }), //pegar o código fonte
+
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact: sourceArtifact,
+                cloudAssemblyArtifact: cloudAssemblyArtifact,
+                // installCommand: 'npx npm@7 install && npm install -g aws-cdk' // npm version 7 in machine
+                installCommand: 'npm install -g aws-cdk',
+                buildCommand: 'npm run build',
+                environment: {
+                    privileged: true, // necessário para executar o docker no processo de build
+                }
+            }), // gerar o código fonte (compilar)
+
+            // TODO add pipeline stages
+        })
+    }
+}
